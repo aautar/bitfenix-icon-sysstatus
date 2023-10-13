a3e139f239de2cc0cc9837960048ec6d0acbe9e4
Avishkar Autar <aautar@digital-radiation.com>
2023-10-12 23:58:58 -0400

    tweak call to http_get()

--- src/web.rs
+++ src/web.rs
@@ -1,4 +1,4 @@
 pub fn http_get() -> String {
-    let sc = String::from(reqwest::blocking::get("https://scratchgraph.com/healthz")?.status().as_str());
+    let sc = String::from(reqwest::blocking::get("https://scratchgraph.com/healthz").unwrap().status().as_str());
     sc
 }
